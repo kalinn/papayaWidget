50b8d00ad72aa2fee274400f123a1ff75d610890
Kristin <kalinn@ncsu.edu>
2017-05-01 17:59:17 -0400

    change height

--- R/papayaWidget.R
+++ R/papayaWidget.R
@@ -70,7 +70,7 @@ papayaWidget <- function(
 #' @name papayaWidget-shiny
 #'
 #' @export
-papayaWidgetOutput <- function(outputId, width = '100%', height = '600px'){
+papayaWidgetOutput <- function(outputId, width = '100%', height = '1200px'){
   htmlwidgets::shinyWidgetOutput(outputId, "papayaWidget", width,
                                  height, package = "papayaWidget")
 }
